d5e9d3442d179f041f463717b14887d8ab880422
barbaracwx <barbara9668@gmail.com>
2025-03-05 16:18:22 +0800

    remove psycorp

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import os
-import psycopg2
 from dotenv import load_dotenv
 from pymongo import MongoClient  # Import MongoDB
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
